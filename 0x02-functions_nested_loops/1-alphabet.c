6451e59600fd59809b7dbccee1045be112184cd6
necxus <necxusandrew@gmail.comgit config --global user.name necxus>
2022-12-13 05:06:52 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,11 @@
 /**
  * print lower case followed by a line
  **/
-void print_alphabeti(void)
+void print_alphabet(void);
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i='a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
