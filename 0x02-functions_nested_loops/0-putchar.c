5c94d68e1a76968acd2226bba65400f94be549c4
necxus <necxusandrew@gmail.comgit config --global user.name necxus>
2022-12-14 01:31:16 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main
+* main - prints _putchar, followed by a new line.
 * Return: 0 always
 **/
 
@@ -16,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
